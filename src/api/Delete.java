33caa35548658fbf0f832b2eaa140717bd16d1c2
noShampoo <2551480456@qq.com>
2020-12-17 18:42:36 +0800

    test resultFul

--- src/api/Delete.java
+++ src/api/Delete.java
@@ -1,6 +1,7 @@
 package api;
 
 import JdbcUtils.JDBCUtils;
+import api.util.ResultUtil;
 import dao.Create;
 import dao.QianDao;
 import domain.User;
@@ -14,6 +15,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
@@ -26,16 +28,38 @@ public class Delete extends HttpServlet {
         String gh = request.getParameter("gh");
 
         if ((gh == null || gh.length() == 0) || (gh.length() == 10 && Integer.parseInt(gh.substring(0, 2)) >= 16)){
-            response.sendRedirect("/failedGh.html");
+            response.setStatus(200);
+            ResultUtil resultUtil = new ResultUtil();
+            resultUtil.setCode(1001);
+            resultUtil.setSuccess(false);
+            resultUtil.setMessage("failedGh.html");
+            PrintWriter writer = response.getWriter();
+            writer.print(resultUtil.toString());
+//            response.sendRedirect("/failedGh.html");
         } else {
             JdbcTemplate template = new JdbcTemplate(JDBCUtils.getDatasource());
             int count = template.update("delete from user where gh = ?", gh);
             if(count >= 1){
                 template.update("insert into logs values(?, ?, ?)", "学号:" + gh, "删除成功", new Date());
-                response.sendRedirect("/deleteSuccess.html");
+                response.setStatus(200);
+                ResultUtil resultUtil = new ResultUtil();
+                resultUtil.setCode(1000);
+                resultUtil.setSuccess(true);
+                resultUtil.setMessage("deleteSuccess.html");
+                PrintWriter writer = response.getWriter();
+                writer.print(resultUtil.toString());
+
+//                response.sendRedirect("/deleteSuccess.html");
             } else {
                 template.update("insert into logs values(?, ?, ?)", "学号:" + gh, "删除失败", new Date());
-                response.sendRedirect("/deleteFailed.html");
+                response.setStatus(200);
+                ResultUtil resultUtil = new ResultUtil();
+                resultUtil.setCode(1001);
+                resultUtil.setSuccess(false);
+                resultUtil.setMessage("deleteFailed.html");
+                PrintWriter writer = response.getWriter();
+                writer.print(resultUtil.toString());
+//                response.sendRedirect("/deleteFailed.html");
             }
         }
     }
