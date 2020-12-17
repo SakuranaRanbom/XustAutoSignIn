368d201edbca394732fe8538ca3b0f959e8c1c7f
noShampoo <2551480456@qq.com>
2020-12-17 17:24:57 +0800

    test resultFul

---
+++ src/api/util/ResultUtil.java
@@ -0,0 +1,34 @@
+package api.util;
+
+public class ResultUtil {
+
+    private Integer code;
+
+    private String message;
+
+    private boolean isSuccess;
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isSuccess() {
+        return isSuccess;
+    }
+
+    public void setSuccess(boolean success) {
+        isSuccess = success;
+    }
+}
