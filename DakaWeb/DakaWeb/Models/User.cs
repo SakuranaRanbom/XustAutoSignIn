d82f3dfd1d617142bf99faafc195a951a43b7564
leoranbom <cmcho@qq.com>
2021-03-12 12:14:27 +0800

    Use blazor wasm to build the html

---
+++ DakaWeb/DakaWeb/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace DakaWeb.Models
+{
+    public class User
+    {
+        public string url { get; set; }
+        public string sno { get; set; }
+        public string email { get; set; }
+        public string verifyCode { get; set; }
+    }
+}
